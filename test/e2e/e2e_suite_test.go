2bf3afd5db9a8e5c98c0242d90126f766b0421b1
Ella <elmatuso@redhat.com>
2026-01-19 16:16:11 +0200

    fix: remove undefined cert-manager references from e2e_suite_test.go

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -59,9 +59,5 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
-	// Teardown CertManager after the suite if not skipped and if it was not already installed
-	if !skipCertManagerInstall && !isCertManagerAlreadyInstalled {
-		_, _ = fmt.Fprintf(GinkgoWriter, "Uninstalling CertManager...\n")
-		utils.UninstallCertManager()
-	}
+	// Cleanup is handled in the test's AfterAll
 })
