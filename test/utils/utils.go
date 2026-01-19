f1572ee3bcec15101c8b172451346de4ca2f4669
Ella <elmatuso@redhat.com>
2026-01-19 15:30:04 +0200

    fix: remove unused warnError function

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -32,10 +32,6 @@ const (
 	defaultKindCluster = "kind"
 )
 
-func warnError(err error) {
-	_, _ = fmt.Fprintf(GinkgoWriter, "warning: %v\n", err)
-}
-
 func Run(cmd *exec.Cmd) (string, error) {
 	dir, _ := GetProjectDir()
 	cmd.Dir = dir
